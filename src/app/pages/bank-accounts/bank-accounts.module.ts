87384728c1f2ba18b5b5bb8ca43dd5c58770f099
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-16 17:45:29 -0500

    Se hace cambios en el diseño, se muestra informacion de las cuentas en la vista

--- src/app/pages/bank-accounts/bank-accounts.module.ts
+++ src/app/pages/bank-accounts/bank-accounts.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { BankAccountsPageRoutingModule } from './bank-accounts-routing.module';
 
 import { BankAccountsPage } from './bank-accounts.page';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BankAccountsPageRoutingModule
+    BankAccountsPageRoutingModule,
+    PipesModule
   ],
   declarations: [BankAccountsPage]
 })
--- src/app/pages/bank-accounts/bank-accounts.page.ts
+++ src/app/pages/bank-accounts/bank-accounts.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ViewDidEnter, MenuController, ModalController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { ViewDidEnter, MenuController, ModalController, ActionSheetController } from '@ionic/angular';
 import { ModalCreateAccountBankComponent } from '../../components/modal-create-account-bank/modal-create-account-bank.component';
 import { BankAccountService } from '../../services/bank-account/bank-account.service';
 
@@ -11,11 +11,13 @@ import { BankAccountService } from '../../services/bank-account/bank-account.ser
 export class BankAccountsPage implements ViewDidEnter {
 
     listAccounts = [];
+    typeAccountView: 'savings' | 'current' = 'savings';
 
     constructor(
         private menuCtrl: MenuController,
         private modalCtrl: ModalController,
-        private bankAccountService: BankAccountService
+        private bankAccountService: BankAccountService,
+        private actionSheetCtrl: ActionSheetController
     ) {}
 
     ionViewDidEnter() {
@@ -36,11 +38,33 @@ export class BankAccountsPage implements ViewDidEnter {
                 console.log(data);
                 this.bankAccountService.createAccount(data).subscribe(resp => {
                     console.log(resp);
-                    this.listAccounts.push(...resp);
+                    this.listAccounts.push(resp);
                 })
             }
         });
 
         return await modal.present();
+    }
+
+
+    async openActionSheetDetailAccount(account) {
+        const actionSheet = await this.actionSheetCtrl.create({
+            header: 'Acciones',
+            buttons: [
+                {
+                    text: 'Movimientos',
+                    icon: 'newspaper-outline',
+                    handler: () => {
+                        console.log('Log');
+                    }
+                }
+            ]
+        });
+
+        await actionSheet.present();
+    }
+
+    segmentChanged(ev) {
+        console.log(ev);
     }
 }
